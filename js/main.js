3e2197bc29cd6991de36c0344aadac8e53fe3fcd
Stanislav <127565688+StanislavKruvianchuk@users.noreply.github.com>
2024-02-18 15:00:41 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,123 @@
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const taskList = document.querySelector('#tasksList');
+const emptyList = document.querySelector("#emptyList");
+
+let tasks = []; 
+
+if(localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+const renderTask = (task) => {
+    const cssClass = task.done ? "task-title task-title--done" : "task-title"
+
+    const taskHTML = `
+    <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+        <span class="${cssClass}">${task.text}</span>
+        <div class="task-item__buttons">
+            <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+            </button>
+            <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+            </button>
+        </div>
+    </li>`
+
+    taskList.insertAdjacentHTML('beforeend', taskHTML)
+}
+
+tasks.forEach((task) => {
+    renderTask(task)
+})
+
+const addTask = (event) => {
+    event.preventDefault();
+
+    const taskText = taskInput.value
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false,
+    }
+
+    tasks.push(newTask)
+    saveToLocalStorage();
+
+    renderTask(newTask);
+    
+    taskInput.value = ""
+    taskInput.focus()
+
+    checkEmptyList();
+}
+
+const deleteTask = (event) => {
+    if (event.target.dataset.action !== 'delete') {
+        return
+    }
+
+    const parentNode = event.target.closest('.list-group-item')
+
+    const id = Number(parentNode.id);
+    tasks = tasks.filter((task) => {
+        return task.id !== id
+    })
+    saveToLocalStorage();
+
+    parentNode.remove()  
+    checkEmptyList()  
+}
+
+const doneTask = (event) => {
+    if(event.target.dataset.action !== 'done') {
+        return
+    }
+
+    const parentNode = event.target.closest('.list-group-item')
+    
+    const id = Number(parentNode.id);
+    
+    const task = tasks.find((task) => {
+        if(task.id === id) {
+            return true
+        }
+    })
+    task.done = !task.done
+
+    saveToLocalStorage();
+
+    const taskTitle = parentNode.querySelector('.task-title')
+    taskTitle.classList.toggle('task-title--done')
+}
+
+const checkEmptyList = () => {
+    if(tasks.length === 0) {
+        const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+					<img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+					<div class="empty-list__title">To-do list is empty</div>
+				</li>`;
+        taskList.insertAdjacentHTML('afterbegin', emptyListHTML)
+    }
+    
+    if(tasks.length > 0) {
+        const emptyListEl = document.querySelector('#emptyList')
+        if(emptyListEl) {
+            emptyListEl.remove()
+        }
+    }
+
+}
+
+const saveToLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+
+checkEmptyList();
+taskList.addEventListener('click', deleteTask)
+taskList.addEventListener('click', doneTask)
+form.addEventListener('submit', addTask);
+    
